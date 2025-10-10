0c91999ddd4c6679142f5245e115118158bc6d3c
theluchadora <160585859+theluchadora@users.noreply.github.com>
2025-10-10 23:34:30 +0300

    feat(api): add urls and viewsets for users and tasks with JWT auth and custom task actions

--- taskmanager/tasks/permissions.py
+++ taskmanager/tasks/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 
+# allow only object owners to access or modify their data
 class IsOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
